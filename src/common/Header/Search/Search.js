da42baf5dc11d1ba52e2e27cc7b9a80b375c41e5
Nickita Shevkunov <shevkunovnikita97@gmail.com>
2021-07-02 06:45:32 +0500

    styling the search field

--- src/common/Header/Search/Search.js
+++ src/common/Header/Search/Search.js
@@ -1,9 +1,10 @@
 import React from "react"
+import styles from "../../scss-blocks/Header/Search.module.scss"
 
 export default function Search () {
   return (
     <>
-     <input />
+     <input className={styles.search}placeholder="Поиск по блогу"/>
     </>
   )
 }
