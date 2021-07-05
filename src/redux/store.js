87f745dcf2608c65d84e851d96145fbcac872f4b
Nickita Shevkunov <shevkunovnikita97@gmail.com>
2021-07-05 19:58:44 +0500

    creating a store object

---
+++ src/redux/store.js
@@ -0,0 +1,47 @@
+import srcPost from "../img/post/Conference.jpg";
+import srcText from "../img/text-page/code.jpg";
+
+export let store = {
+  _state: {
+    home: {
+      posts : [
+        {id: 1, title: "Как я сходил на FrontEnd Conf 2021", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.", img: srcPost, publicDate: "1.07.2021", category: "создание сайтов", link: "читать"}
+    ],
+      notes : [],
+      textNotes: ""
+    },
+    textPage: {
+      arcticle: [
+        {
+        id: 1, title: "Как я сходил на FrontEnd Conf 2021", paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.", img: srcText,publicDate: "1.07.2021", category: "создание сайтов"
+        }
+      ]
+    }
+  },
+  getState() {
+    debugger
+    return this._state
+  },
+  _callSubscribe () {
+      console.log("state changed")
+    },
+  updateNotesText(textNotes) {
+      debugger
+      this._state.home.textNotes = textNotes
+      this._callSubscribe()
+  },
+  addNotes() {
+    let newNotes = {
+      id: Date.now(),
+      text: this._state.home.textNotes
+    }
+    this._state.home.notes.push(newNotes)
+    this._state.home.textNotes = ""
+    this._callSubscribe()
+  },
+  subscribe(observer) {
+    this._callSubscribe = observer  // observer
+  }
+}
+
+window.store = store
