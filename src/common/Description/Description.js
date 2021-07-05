c390d5d6632297e2f772b5714faa526f0a7cfb51
Nickita Shevkunov <shevkunovnikita97@gmail.com>
2021-07-05 09:49:26 +0500

    refactoring posts: moving logic to the Posts component

--- src/common/Description/Description.js
+++ src/common/Description/Description.js
@@ -2,8 +2,6 @@ import React from "react"
 import SubTitle from "./SubTitle"
 import Title from "./Title"
 
-
-
 export default function Description (props) {
 
   return (
--- src/сomponents/Home/Home.js
+++ src/сomponents/Home/Home.js
@@ -1,14 +1,14 @@
 import React from "react"
 import CategoriesArticles from "./CategoriesArticle/CategoriesArticles"
 import Notes from "./Notes/Notes"
-import Post from "./Post/Post"
+import Posts from "./Posts/Posts"
 
 export default function Home () {
   return(
     <div>
       <CategoriesArticles />
       <Notes />
-      <Post />
+      <Posts />
     </div>
   )
 }
