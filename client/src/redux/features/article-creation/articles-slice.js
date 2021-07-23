974a24d7fdbe7ba10d14d6adde346bad849ab666
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-07-23 16:30:10 +0500

    improved article creation logic

--- client/src/redux/features/article-creation/articles-slice.js
+++ client/src/redux/features/article-creation/articles-slice.js
@@ -1,34 +1,70 @@
+import {createArticles} from "../../../api/api" 
+const SET_NEW_ARTICLE_TITLE = "articles/setNewArticleTitle"
 const NEW_ARTICLE_TEXT_UPDATED = "articles/newArticleTextUpdated"
+const NEW_ARTICLE_CATEGORY = "articles/newArticleCategory"
 const ARTICLE_ADDED = "articles/articleAdded"
 
 const initialState = {
-    newArticleText: "",
-    articleBody: []
+    newTitle: "",
+    newText: "",
+    newCategory: "",
+    aricle: {
+        title: "",
+        paragraph: "",
+        publicDate: null,
+        category: ""
+    },
 }       
+
+const update = (state, action, propertyFieldCopy, propertyFieldOrigin) => {
+    return {
+        ...state,
+        [propertyFieldCopy]: [propertyFieldOrigin] = action.payload
+    }
+}
+
+const settignDate = () => {
+    let date = new Date()
+    let datestring  = date.getDate()  + "-" + (date.getMonth()+1) + "-" + date.getFullYear() + " " +
+    date.getHours() + ":" + date.getMinutes();
+    return datestring
+}
 
 const articleCreationReducer = (state = initialState, action) => {
     switch(action.type) {
+        
+        case SET_NEW_ARTICLE_TITLE : {
+            return update(state, action, "newTitle", "state.newTitle")
+            
+        }
         case NEW_ARTICLE_TEXT_UPDATED: {
-            let stateCopy = {...state}
-            stateCopy.newArticleText = {...state.newArticleText}
-            stateCopy.newArticleText = action.payload
-            return stateCopy
+            return update(state, action, "newText", "state.newText")
+        }
+        case NEW_ARTICLE_CATEGORY: {
+            return update(state, action, "newCategory", "state.newCategory")
         }
         case ARTICLE_ADDED:
             let stateCopy = {...state}
             let newArticle = {
-                text: state.newArticleText
+                title: state.newTitle,
+                paragraph: state.newText,
+                publicDate: settignDate(),
+                category: state.newCategory
             }
-            stateCopy.articleBody = [...state.articleBody]
-            stateCopy.articleBody.push(newArticle)
-            stateCopy.newArticleText = ""
+             stateCopy.aricle = newArticle
+            stateCopy.newTitle = ""
+            stateCopy.newText = ""
+            stateCopy.newCategory = ""
             return stateCopy
         default: 
             return state
     }
 }
-
+export const updateArticleTitleAC = (payload) => ({type: SET_NEW_ARTICLE_TITLE, payload})
 export const updateArticleTextAC = (payload) => ({type: NEW_ARTICLE_TEXT_UPDATED, payload})
+export const updateArticleCategoryAC = (payload) => ({type: NEW_ARTICLE_CATEGORY, payload})
 export const articleAddedAC = () => ({type: ARTICLE_ADDED})
 
 export default articleCreationReducer
+
+
--- client/src/сomponents/SandBox/SandBoxContainer.js
+++ client/src/сomponents/SandBox/SandBoxContainer.js
@@ -1,18 +1,25 @@
 import { connect } from "react-redux";
-import { articleAddedAC, updateArticleTextAC } from "../../redux/features/article-creation/articles-slice";
+import { articleAddedAC, updateArticleCategoryAC, updateArticleTextAC, updateArticleTitleAC } from "../../redux/features/article-creation/articles-slice";
 import SandBox from "./SandBox";
 
 let mapStateToProps = (state) => {
   return {
-    createArticles : state.createArticles
+    articles : state.articles
   }
 }
 
 let mapDipsatchToProps = (dispatch) => {
   return {
+
+    handleArticleTitle: (title) => {
+      dispatch(updateArticleTitleAC(title))
+    },
     handleArticleBody: (text) => {
       dispatch(updateArticleTextAC(text))
-  },
+    },
+    handleArticleCategory: (category) => {
+    dispatch(updateArticleCategoryAC(category))
+    },
     articlePublication: () => {
       dispatch(articleAddedAC())
     }
