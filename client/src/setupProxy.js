cf6173da7c759448ff3936648e256f644e4a8d83
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-08-10 13:10:50 +0500

    extension of proxying

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,6 @@
 const {createProxyMiddleware}  = require("http-proxy-middleware");
 module.exports = app => {
    app.use('/articles', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }));
+   app.use('/posts', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }));
+   app.use('/notes', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }));
 }
