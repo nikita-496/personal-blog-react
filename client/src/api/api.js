88b7cd73e0d5b08509e861944de6a0a08798833c
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-08-12 11:58:37 +0500

    rewrite to Async / await

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,89 +1,36 @@
 import * as axios from "axios"
-
-/*export const articlesAPI = {
-   createArticles (articleData) {
-      return axios.post("/articles", articleData.articles.article).then(response => response.data)
-   },
-   getArticlesPost (page, limit ) {
-      return axios.get("/select", {
-         params: {
-            page: page,
-            limit: limit,
-         }
-      }).then(response => response)
-   },
-   getArticles() {
-      return axios.get("/articles")
-   },
-   getArticle (articleId) {
-      return axios.get(`/articles/${articleId}`)
-   }
-}
-
-export const notesAPI = {
-   createNotes (notesData) {
-      return axios.post("/notes", notesData.notes.noteItems).then(response => response.data)
-   },
-   getNotes () {
-      return axios.get("/notes")
-   }
-}*/
-
 export class ArticleService {
    static async createArticles (articleData) {
-      try {
-         const response = axios.post("/articles", articleData.articles.article)
-         return response.data
-      }catch (e) {
-         console.log(e)
-      }
+      const response = await axios.post("/articles", articleData.articles.article)
+      return response.data 
    }
    static async getArticlesPost (page, limit ) {
-      try {
-         const response = axios.get("/select", {
-            params: {
-               page: page,
-               limit: limit,
-            }
-         })
-         return response
-      }catch (e) {
-         console.log(e)
+    const response = await axios.get("/select", {
+      params: {
+         page: page,
+         limit: limit,
       }
+   })
+      return response.data
+      
    }
    static async getArticles() {
-      try {
-         const response = axios.get("/articles")
+         const response = await axios.get("/articles")
          return response
-      }catch (e) {
-         console.log(e)
-      }
    }
    static async getArticle (articleId) {
-      try {
-         const response = axios.get(`/articles/${articleId}`)
-         return response
-      }catch (e) {
-         console.log(e)
-      }
+      const response = await axios.get(`/articles/${articleId}`)
+      return response
    }
 }
 
 export class NotesService {
    static async createNotes (notesData) {
-      try {
-         const response = axios.post("/notes", notesData.notes.noteItems)
-         return response
-      }catch (e) {
-         console.log(e)
-      }
+      const response = await axios.post("/notes", notesData.notes.noteItems)
+      return response
    }
    static async getNotes () {
-      try {
-         const response = axios.get("/notes")
-         return response
-      }catch (e) {
-         console.log(e)
-      }
+      const response = await axios.get("/notes")
+      return response
    }
 }
