851f8e14e54763bffc976f40322be32886129671
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-08-15 09:40:05 +0500

    remove posts routing

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,5 +1,5 @@
 const articles = require("../../controllers/articles")
-const posts = require("../../controllers/posts")
+//const posts = require("../../controllers/posts")
 const notes = require("../../controllers/notes")
 
 module.exports = function(app){
@@ -9,11 +9,11 @@ module.exports = function(app){
   app.put("/articles/:id", articles.update)
   app.delete("/articles/:id", articles.delete)
 
-  app.get("/posts/:id", posts.findById)
+  /*app.get("/posts/:id", posts.findById)
   app.get("/posts", posts.all)
   app.post("/posts", posts.create)
   app.put("/posts/:id", posts.update)
-  app.delete("/posts/:id", posts.delete)
+  app.delete("/posts/:id", posts.delete)*/
 
   app.get("/notes/:id", notes.findById)
   app.get("/notes", notes.all)
