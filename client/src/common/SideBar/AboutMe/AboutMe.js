4905f788db7d888bd4abc5adf2898449bae455ce
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-09-06 13:50:56 +0500

    correct functioning of the field for creating a status

--- client/src/common/SideBar/AboutMe/AboutMe.js
+++ client/src/common/SideBar/AboutMe/AboutMe.js
@@ -1,38 +1,42 @@
 import React, {useEffect, useState} from "react"
 import Text from "../../Text/Text"
 import styles from "../../scss-blocks/SideBar/AboutMe.module.scss"
-import useControlled from "../../../hooks/useControlled"
+import Button from "../../Button/Button"
+import FiledwitHover from "../../Field/FieldWithHover"
 
 
 export default function AboutMe () {
   const [editMode, setEditMode] = useState(false)
-  const [textStatus, setTextSatus] = useState("")
-  const [text, handleText] = useControlled("")
-  
+  const [statusText, setStatusText] = useState("")
 
   useEffect(()=> {
     const data = window.localStorage.getItem("statusText")
-    setTextSatus(JSON.parse(data)) 
+    setStatusText(JSON.parse(data)) 
   }, [])
 
-  const handleStatusText = () => {
+  const handleStatusText = (text) => {
     window.localStorage.setItem("statusText", JSON.stringify(text))
-    setEditMode(!editMode)
   }
+
+  const onButton = () => setEditMode(!editMode)
 
   return (
     <div className={styles.wrapper}>
-      <span className={styles.topLine}></span>
-      <span className={styles.editIcon} onClick={() => setEditMode(!editMode)}></span>
-    {editMode ? <form className={styles.form}>
-      <input className={styles.fieldStatus} type="text" value={text} onChange={handleText}/>
-      <button type="button" onClick={handleStatusText} >сохранить</button>
+      <span className={styles.editIcon} onClick={() => setEditMode(!editMode)}/>
+    {editMode ? <>
+      <form className={styles.form}>
+      <FiledwitHover value={statusText} callback={handleStatusText} 
+      type="text" placeholder="Расскажите о себе ..."/>
       </form>
-      :
-      <Text text={textStatus}>
-      </Text>
+      <Button callback={onButton}>сохранить</Button>
+      </>
+      : <>
+      <span className={styles.topLine}/>
+      <Text text={statusText}/>
+      <span className={styles.bottomLine}></span>
+      </>
     }
-     <span className={styles.bottomLine}></span>
+  
  </div>
   )
 }
--- client/src/hooks/useControlled.js
+++ client/src/hooks/useControlled.js
@@ -1,12 +1,12 @@
 import { useState } from "react"
 
 const useControlled = (state, callback) => {
+  console.log(callback)
   const [text, setText] = useState(state)
 
   const handleText = (e) => {
     if (callback !== undefined) {
       callback(e.target.value)
-      setText(e.target.value)
     }
     setText(e.target.value)
 
